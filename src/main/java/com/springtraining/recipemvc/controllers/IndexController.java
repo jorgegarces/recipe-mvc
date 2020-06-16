d5ac84d0b1114b2034d7e79ccfd5ab2256b7777a
Jorge <dam0ne@hemwick>
2020-06-16 11:48:17 +0200

    implemented lombom annotations

--- src/main/java/com/springtraining/recipemvc/controllers/IndexController.java
+++ src/main/java/com/springtraining/recipemvc/controllers/IndexController.java
@@ -1,14 +1,16 @@
 package com.springtraining.recipemvc.controllers;
 
 import com.springtraining.recipemvc.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
-   private final RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -16,8 +18,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage(Model model) {
-
-model.addAttribute("recipes", recipeService.getRecipes());
+        log.debug("I'm in the controller");
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
